fc6144bb915128d43f4a8e9a71a316b0d9406e6a
mewsu <zeiontic@gmail.com>
2020-08-02 22:40:58 -0400

    add login route & setup passport

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 // ./routes/index.js
 const customer = require("./customer");
 const film = require("./film");
+const login = require("./login");
 // const photos = require("./photos");
 module.exports = app => {
   app.use("/customer", customer);
   app.use("/film", film);
+  app.use("/login", login);
   // app.use("/photos", photos);
   // etc..
 };
